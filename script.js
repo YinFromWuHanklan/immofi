34a3d547af7328d8dd9d981774ed8319d6753659
Yin Yin Wu-Hanke <y.wu-hanke@ellusion.de>
2023-05-23 15:03:59 +0200

    fixed radio input value and add normalize.css

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ kaufpreisrechner.addEventListener('submit', (e) => {
     const ek = document.querySelector('#eigenkapital').value;
     const sz = document.querySelector('#sollzins').value;
     const bl = document.querySelector('#bundesland').value;
-    const mk = document.querySelector('#makler').value;
+    const mk = document.querySelector('input[name="maklerprovision"]:checked').value;
     const tg = document.querySelector('#tilgung').value;
 
     const nk = (+kp)*0.015+(+kp)*0.005+(+kp)*(+mk)+(+kp)*(+bl);
@@ -18,7 +18,7 @@ kaufpreisrechner.addEventListener('submit', (e) => {
 
     const p = document.createElement('p');
     const summary = `
-        <p>Monatliche Belastung: ${mb}€</p>
+        <p class="main-sum">Monatliche Belastung: ${mb}€</p>
         <p>Kaufpreis: ${kp}€</p>
         <p>Kaufnebenkosten: ${nk}€ <span>${nk/kp*100}%</span></p>
         <p>Eigenkapital: ${ek}€ <span>${ek/kp*100}%</span></p>
